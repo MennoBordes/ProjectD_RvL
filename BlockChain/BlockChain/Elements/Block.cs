c17c244e3d103bf39ba51f4690e1bbb13dd30f92
Skillerde6de <mennobordes@hetnet.nl>
2019-02-12 11:09:26 +0100

    Block in blockchain

---
+++ BlockChain/BlockChain/Elements/Block.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BlockChain.Elements
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timestamp, string previousHash, string data)
+        {
+            this.Index = 0;
+            this.TimeStamp = timestamp;
+            this.PreviousHash = previousHash;
+            this.Data = data;
+            this.Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
