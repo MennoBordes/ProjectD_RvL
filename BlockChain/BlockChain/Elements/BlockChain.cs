516a0f9bbf70d93f4ee7d20ceaeed36972eaa9a9
Skillerde6de <mennobordes@hetnet.nl>
2019-02-12 11:16:45 +0100

    BlockChain chain

---
+++ BlockChain/BlockChain/Elements/BlockChain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain.Elements
+{
+    class BlockChain
+    {
+        public IList<Block> Chain { get; set; }
+        public BlockChain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+    }
+}
