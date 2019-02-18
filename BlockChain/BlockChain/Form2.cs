29cf7717bd628463bf9e89e3fa6b9a10735a219b
Skillerde6de <mennobordes@hetnet.nl>
2019-02-18 10:48:17 +0100

    Update Form2.cs

--- BlockChain/BlockChain/Form2.cs
+++ BlockChain/BlockChain/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BlockChain.Form2Elements;
 
 namespace BlockChain
 {
@@ -16,5 +17,6 @@ namespace BlockChain
         {
             InitializeComponent();
         }
+
     }
 }
