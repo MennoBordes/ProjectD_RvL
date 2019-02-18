38924e0e48ef86b1f0a1c07a305570e1374a041e
Skillerde6de <mennobordes@hetnet.nl>
2019-02-18 10:29:53 +0100

    Renaming folder

--- BlockChain/BlockChain/Form1.cs
+++ BlockChain/BlockChain/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BlockChain.Elements;
+using BlockChain.Form1Elements;
 using Newtonsoft.Json;
 
 namespace BlockChain
