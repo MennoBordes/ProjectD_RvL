39fa83872604f4c1a6713df22c1ab948dc1ebdf8
Skillerde6de <mennobordes@hetnet.nl>
2019-03-11 10:54:30 +0100

    Redesigned the tester

--- BlockChain/BlockChain/Form3.Designer.cs
+++ BlockChain/BlockChain/Form3.Designer.cs
@@ -28,63 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(194, 65);
-			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(159, 19);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "First blockchain example";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(194, 105);
-			this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(159, 19);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "Second blockchain example";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(194, 144);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(159, 23);
-			this.button3.TabIndex = 2;
-			this.button3.Text = "Text to hash code";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// Form3
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(600, 366);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.Name = "Form3";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form3";
-			this.ResumeLayout(false);
+      this.button1 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(259, 121);
+      this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(239, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Insert fake data into blockchain";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(259, 177);
+      this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(239, 28);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "Get the hash value for a given text";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // Form3
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button1);
+      this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.Name = "Form3";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form3";
+      this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button3;
 	}
 }
