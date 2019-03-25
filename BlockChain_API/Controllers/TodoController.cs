6689f62b728e14114d147f2ab5a2308d7604a7da
Skillerde6de <mennobordes@hetnet.nl>
2019-03-25 12:27:15 +0100

    todoSetup

---
+++ BlockChain_API/Controllers/TodoController.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlockChain_API.Models;
+
+namespace BlockChain_API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TodoController : ControllerBase
+  {
+    private readonly TodoContext _context;
+
+    public TodoController(TodoContext context)
+    {
+      _context = context;
+
+      if (_context.TodoItems.Count() == 0)
+      {
+        // Create a new TodoItem if collection is empty,
+        // which means you can't delete all TodoItems.
+        _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+        _context.SaveChanges();
+      }
+    }
+
+
+    // GET: api/Todo
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+      return await _context.TodoItems.ToListAsync();
+    }
+
+    // GET: api/Todo/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+      var todoItem = await _context.TodoItems.FindAsync(id);
+
+      if (todoItem == null)
+      {
+        return NotFound();
+      }
+
+      return todoItem;
+    }
+
+    // POST: api/Todo
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    {
+      _context.TodoItems.Add(item);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+    }
+
+    // PUT: api/Todo/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+    {
+      if (id != item.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(item).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    // DELETE: api/Todo/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(long id)
+    {
+      var todoItem = await _context.TodoItems.FindAsync(id);
+
+      if (todoItem == null)
+      {
+        return NotFound();
+      }
+
+      _context.TodoItems.Remove(todoItem);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
---
+++ BlockChain_API/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace BlockChain_API.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+    }
+}
