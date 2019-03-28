e3b30f6e4abc338891bbc2bff6234f3647037437
Skillerde6de <mennobordes@hetnet.nl>
2019-03-28 11:31:41 +0100

    Created function for hash conversion.

--- BlockChain/BlockChain/Form4.cs
+++ BlockChain/BlockChain/Form4.cs
@@ -14,13 +14,11 @@ namespace BlockChain
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			SHA256 sha256 = SHA256.Create();
-			byte[] inputBytes = Encoding.ASCII.GetBytes(textBox1.Text);
-			byte[] outputBytes = sha256.ComputeHash(inputBytes);
-			textBox2.Text = Convert.ToBase64String(outputBytes);
-			string res = string.Empty;
-			foreach(byte b in outputBytes)
-			{
+      byte[] hash = CreateHash(textBox1.Text);
+      textBox2.Text = Convert.ToBase64String(hash);
+      string res = string.Empty;
+      foreach (byte b in hash)
+      {
 				res = res + b + " ";
 			}
 			textBox3.Text = res;
@@ -28,16 +26,26 @@ namespace BlockChain
 
     private void button2_Click(object sender, EventArgs e)
     {
-      SHA256 sha256 = SHA256.Create();
-      byte[] inputbytes = Encoding.ASCII.GetBytes(textBox6.Text);
-      byte[] outputBytes = sha256.ComputeHash(inputbytes);
-      textBox5.Text = Convert.ToBase64String(outputBytes);
+      byte[] hash = CreateHash(textBox6.Text);
+      textBox5.Text = Convert.ToBase64String(hash);
       string res = string.Empty;
-      foreach (byte b in outputBytes)
+      foreach (byte b in hash)
       {
         res = res + b + " ";
       }
       textBox4.Text = res;
+    }
+
+    /// <summary>
+    /// Takes as input a string and returns the hashcode for that given string
+    /// </summary>
+    /// <param name="text">The string to convert to a byte[]</param>
+    /// <returns></returns>
+    private byte[] CreateHash(string text)
+    {
+      SHA256 sha256 = SHA256.Create();
+      byte[] input = Encoding.ASCII.GetBytes(text);
+      return sha256.ComputeHash(input);
     }
   }
 }
