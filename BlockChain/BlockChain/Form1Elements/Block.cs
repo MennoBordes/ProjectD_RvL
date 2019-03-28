af18a7c52c9a4179efd4545781ce4854e4ce3943
Skillerde6de <mennobordes@hetnet.nl>
2019-03-28 11:41:27 +0100

    Added descriptions for all functions.

--- BlockChain/BlockChain/Form1Elements/Block.cs
+++ BlockChain/BlockChain/Form1Elements/Block.cs
@@ -4,14 +4,42 @@ using System.Security.Cryptography;
 
 namespace BlockChain.Form1Elements
 {
+  /// <summary>
+  /// The block
+  /// </summary>
   public class Block
   {
+    /// <summary>
+    /// The index of the block in the chain.
+    /// </summary>
     public int Index { get; set; }
+
+    /// <summary>
+    /// The timestamp at which the block was created.
+    /// </summary>
     public DateTime TimeStamp { get; set; }
+
+    /// <summary>
+    /// The hash of the block before the current block.
+    /// </summary>
     public string PreviousHash { get; set; }
+
+    /// <summary>
+    /// The hash of the current block.
+    /// </summary>
     public string Hash { get; set; }
+    
+    /// <summary>
+    /// The data stored in the current block.
+    /// </summary>
     public string Data { get; set; }
 
+    /// <summary>
+    /// The constructor of the block
+    /// </summary>
+    /// <param name="timestamp">The timestamp at which the block is created</param>
+    /// <param name="previousHash">The hash of the previous block</param>
+    /// <param name="data">The data to be stored in the current block</param>
     public Block(DateTime timestamp, string previousHash, string data)
     {
       this.Index = 0;
@@ -21,6 +49,10 @@ namespace BlockChain.Form1Elements
       this.Hash = CalculateHash();
     }
 
+    /// <summary>
+    /// Calculates the hash of the current block
+    /// </summary>
+    /// <returns></returns>
     public string CalculateHash()
     {
       SHA256 sha256 = SHA256.Create();
--- BlockChain/BlockChain/Form1Elements/BlockChain.cs
+++ BlockChain/BlockChain/Form1Elements/BlockChain.cs
@@ -3,35 +3,63 @@ using System.Collections.Generic;
 
 namespace BlockChain.Form1Elements
 {
+  /// <summary>
+  /// The blockchain for the RvL blockchain project.
+  /// </summary>
   public class RvLBlockChain
   {
+    /// <summary>
+    /// The chain of blocks.
+    /// </summary>
     public IList<Block> Chain { get; set; }
+
+    /// <summary>
+    /// The constructor for the chain.
+    /// </summary>
     public RvLBlockChain()
     {
       InitializeChain();
       AddGenesisBlock();
     }
 
+    /// <summary>
+    /// Initializes the chain.
+    /// </summary>
     public void InitializeChain()
     {
       Chain = new List<Block>();
     }
 
+    /// <summary>
+    /// Creates the genesis block.
+    /// </summary>
+    /// <returns></returns>
     public Block CreateGenesisBlock()
     {
       return new Block(DateTime.Now, null, "{}");
     }
 
+    /// <summary>
+    /// Adds the genesisblock to the chain.
+    /// </summary>
     public void AddGenesisBlock()
     {
       Chain.Add(CreateGenesisBlock());
     }
 
+    /// <summary>
+    /// Returns the most recent block.
+    /// </summary>
+    /// <returns></returns>
     public Block GetLatestBlock()
     {
       return Chain[Chain.Count - 1];
     }
 
+    /// <summary>
+    /// Adds the given block to the chain.
+    /// </summary>
+    /// <param name="block">The block to add to the chain.</param>
     public void AddBlock(Block block)
     {
       if (Chain.Count > 1)
@@ -47,6 +75,11 @@ namespace BlockChain.Form1Elements
       block.Hash = block.CalculateHash();
       Chain.Add(block);
     }
+
+    /// <summary>
+    /// Checks whether the chain hash order is valid.
+    /// </summary>
+    /// <returns></returns>
     public bool IsValid()
     {
       for (int i = 1; i < Chain.Count; i++)
