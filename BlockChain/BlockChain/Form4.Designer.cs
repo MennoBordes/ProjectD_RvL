643b00db930fea03cf20c6fa241958e472adb919
Skillerde6de <mennobordes@hetnet.nl>
2019-03-28 11:22:11 +0100

    Added secondary input for comparison

--- BlockChain/BlockChain/Form4.Designer.cs
+++ BlockChain/BlockChain/Form4.Designer.cs
@@ -35,12 +35,23 @@
       this.textBox3 = new System.Windows.Forms.TextBox();
       this.label3 = new System.Windows.Forms.Label();
       this.label4 = new System.Windows.Forms.Label();
+      this.button2 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.textBox6 = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(175, 194);
-      this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button1.Location = new System.Drawing.Point(113, 49);
+      this.button1.Margin = new System.Windows.Forms.Padding(4);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(117, 25);
       this.button1.TabIndex = 0;
@@ -50,8 +61,8 @@
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(304, 194);
-      this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.textBox1.Location = new System.Drawing.Point(242, 52);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(4);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(389, 22);
       this.textBox1.TabIndex = 1;
@@ -59,7 +70,7 @@
       // label2
       // 
       this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(86, 360);
+      this.label2.Location = new System.Drawing.Point(27, 163);
       this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(206, 17);
@@ -68,8 +79,8 @@
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(300, 360);
-      this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.textBox2.Location = new System.Drawing.Point(241, 160);
+      this.textBox2.Margin = new System.Windows.Forms.Padding(4);
       this.textBox2.Name = "textBox2";
       this.textBox2.ReadOnly = true;
       this.textBox2.Size = new System.Drawing.Size(389, 22);
@@ -77,8 +88,8 @@
       // 
       // textBox3
       // 
-      this.textBox3.Location = new System.Drawing.Point(304, 270);
-      this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.textBox3.Location = new System.Drawing.Point(241, 93);
+      this.textBox3.Margin = new System.Windows.Forms.Padding(4);
       this.textBox3.Multiline = true;
       this.textBox3.Name = "textBox3";
       this.textBox3.ReadOnly = true;
@@ -88,40 +99,139 @@
       // label3
       // 
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(304, 170);
+      this.label3.Location = new System.Drawing.Point(242, 28);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(452, 17);
+      this.label3.Size = new System.Drawing.Size(352, 17);
       this.label3.TabIndex = 6;
-      this.label3.Text = "Voer hieronder de waardes in waarvan je de hashcode wilt berekenen.";
+      this.label3.Text = "Voer hier de waarde in om de hashcode te berekenen.";
       // 
       // label4
       // 
       this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(60, 270);
+      this.label4.Location = new System.Drawing.Point(7, 93);
       this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
       this.label4.Name = "label4";
       this.label4.Size = new System.Drawing.Size(233, 17);
       this.label4.TabIndex = 7;
       this.label4.Text = "De byte output van de hash functie:";
       // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(113, 50);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(117, 23);
+      this.button2.TabIndex = 8;
+      this.button2.Text = "Creëer Hash";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(238, 25);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(352, 17);
+      this.label1.TabIndex = 9;
+      this.label1.Text = "Voer hier de waarde in om de hashcode te berekenen.";
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(241, 92);
+      this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+      this.textBox4.Multiline = true;
+      this.textBox4.Name = "textBox4";
+      this.textBox4.ReadOnly = true;
+      this.textBox4.Size = new System.Drawing.Size(389, 47);
+      this.textBox4.TabIndex = 12;
+      // 
+      // textBox5
+      // 
+      this.textBox5.Location = new System.Drawing.Point(241, 159);
+      this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+      this.textBox5.Name = "textBox5";
+      this.textBox5.ReadOnly = true;
+      this.textBox5.Size = new System.Drawing.Size(389, 22);
+      this.textBox5.TabIndex = 11;
+      // 
+      // textBox6
+      // 
+      this.textBox6.Location = new System.Drawing.Point(242, 51);
+      this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+      this.textBox6.Name = "textBox6";
+      this.textBox6.Size = new System.Drawing.Size(389, 22);
+      this.textBox6.TabIndex = 10;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(7, 92);
+      this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(233, 17);
+      this.label5.TabIndex = 14;
+      this.label5.Text = "De byte output van de hash functie:";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(27, 162);
+      this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(206, 17);
+      this.label6.TabIndex = 13;
+      this.label6.Text = "De Hashwaarde in tekst format:";
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.label5);
+      this.groupBox1.Controls.Add(this.button2);
+      this.groupBox1.Controls.Add(this.label6);
+      this.groupBox1.Controls.Add(this.label1);
+      this.groupBox1.Controls.Add(this.textBox4);
+      this.groupBox1.Controls.Add(this.textBox6);
+      this.groupBox1.Controls.Add(this.textBox5);
+      this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.groupBox1.Location = new System.Drawing.Point(0, 224);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(662, 197);
+      this.groupBox1.TabIndex = 15;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Genereer een hashcode voor de gegeven input, ter vergelijking.";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.button1);
+      this.groupBox2.Controls.Add(this.textBox1);
+      this.groupBox2.Controls.Add(this.label4);
+      this.groupBox2.Controls.Add(this.label2);
+      this.groupBox2.Controls.Add(this.label3);
+      this.groupBox2.Controls.Add(this.textBox2);
+      this.groupBox2.Controls.Add(this.textBox3);
+      this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+      this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.groupBox2.Location = new System.Drawing.Point(0, 0);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(662, 207);
+      this.groupBox2.TabIndex = 16;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Genereer een hashcode voor de gegeven input.";
+      // 
       // Form4
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(1067, 554);
-      this.Controls.Add(this.label4);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.textBox3);
-      this.Controls.Add(this.textBox2);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.button1);
-      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.ClientSize = new System.Drawing.Size(662, 421);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.groupBox1);
+      this.Margin = new System.Windows.Forms.Padding(4);
       this.Name = "Form4";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Form4";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox2.PerformLayout();
       this.ResumeLayout(false);
-      this.PerformLayout();
 
 		}
 
@@ -134,5 +244,14 @@
 		private System.Windows.Forms.TextBox textBox3;
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.TextBox textBox6;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.GroupBox groupBox2;
   }
 }
--- BlockChain/BlockChain/Form4.cs
+++ BlockChain/BlockChain/Form4.cs
@@ -25,5 +25,19 @@ namespace BlockChain
 			}
 			textBox3.Text = res;
 		}
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      SHA256 sha256 = SHA256.Create();
+      byte[] inputbytes = Encoding.ASCII.GetBytes(textBox6.Text);
+      byte[] outputBytes = sha256.ComputeHash(inputbytes);
+      textBox5.Text = Convert.ToBase64String(outputBytes);
+      string res = string.Empty;
+      foreach (byte b in outputBytes)
+      {
+        res = res + b + " ";
+      }
+      textBox4.Text = res;
+    }
   }
 }
