a9057692d96aebe5a3e9ac3d5404aab883fae7a5
Skillerde6de <mennobordes@hetnet.nl>
2019-04-04 12:11:06 +0200

    Moved the DGV's to a new pos. Added labels for description

--- BlockChain/BlockChain/Form1.Designer.cs
+++ BlockChain/BlockChain/Form1.Designer.cs
@@ -28,78 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 260);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Insert Fake Data";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(226, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(918, 240);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(226, 280);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(918, 264);
-            this.dataGridView2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Original Chain";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 306);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Altered Chain";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 566);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.button1 = new System.Windows.Forms.Button();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.dataGridView2 = new System.Windows.Forms.DataGridView();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.LblDataAltered = new System.Windows.Forms.Label();
+      this.LblValid = new System.Windows.Forms.Label();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.LblInvalid = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.panel1.SuspendLayout();
+      this.panel2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(7, 307);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(262, 27);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Foutieve data in een block opslaan";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.dataGridView1.Enabled = false;
+      this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersVisible = false;
+      this.dataGridView1.RowTemplate.Height = 24;
+      this.dataGridView1.Size = new System.Drawing.Size(1476, 250);
+      this.dataGridView1.TabIndex = 1;
+      // 
+      // dataGridView2
+      // 
+      this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+      this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.dataGridView2.Enabled = false;
+      this.dataGridView2.Location = new System.Drawing.Point(3, 18);
+      this.dataGridView2.Name = "dataGridView2";
+      this.dataGridView2.RowHeadersVisible = false;
+      this.dataGridView2.RowTemplate.Height = 24;
+      this.dataGridView2.Size = new System.Drawing.Size(1476, 232);
+      this.dataGridView2.TabIndex = 2;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.panel1);
+      this.groupBox1.Controls.Add(this.dataGridView1);
+      this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.groupBox1.Location = new System.Drawing.Point(0, 0);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(1482, 271);
+      this.groupBox1.TabIndex = 5;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "De originele BlockChain";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.panel2);
+      this.groupBox2.Controls.Add(this.dataGridView2);
+      this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.groupBox2.Location = new System.Drawing.Point(0, 352);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(1482, 253);
+      this.groupBox2.TabIndex = 6;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "De aangepaste BlockChain";
+      // 
+      // LblDataAltered
+      // 
+      this.LblDataAltered.AutoSize = true;
+      this.LblDataAltered.Location = new System.Drawing.Point(12, 287);
+      this.LblDataAltered.Name = "LblDataAltered";
+      this.LblDataAltered.Size = new System.Drawing.Size(46, 17);
+      this.LblDataAltered.TabIndex = 7;
+      this.LblDataAltered.Text = "label1";
+      // 
+      // LblValid
+      // 
+      this.LblValid.AutoSize = true;
+      this.LblValid.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.LblValid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.LblValid.Location = new System.Drawing.Point(0, 0);
+      this.LblValid.Name = "LblValid";
+      this.LblValid.Size = new System.Drawing.Size(59, 20);
+      this.LblValid.TabIndex = 2;
+      this.LblValid.Text = "label1";
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+      this.panel1.Controls.Add(this.LblValid);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(3, 244);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(1476, 24);
+      this.panel1.TabIndex = 3;
+      // 
+      // panel2
+      // 
+      this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+      this.panel2.Controls.Add(this.LblInvalid);
+      this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel2.Location = new System.Drawing.Point(3, 226);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(1476, 24);
+      this.panel2.TabIndex = 4;
+      // 
+      // LblInvalid
+      // 
+      this.LblInvalid.AutoSize = true;
+      this.LblInvalid.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.LblInvalid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.LblInvalid.Location = new System.Drawing.Point(0, 0);
+      this.LblInvalid.Name = "LblInvalid";
+      this.LblInvalid.Size = new System.Drawing.Size(59, 20);
+      this.LblInvalid.TabIndex = 2;
+      this.LblInvalid.Text = "label1";
+      this.LblInvalid.Visible = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1482, 605);
+      this.Controls.Add(this.LblDataAltered);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      this.panel2.ResumeLayout(false);
+      this.panel2.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
@@ -108,8 +188,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-    }
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Label LblDataAltered;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Label LblValid;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.Label LblInvalid;
+  }
 }
 
--- BlockChain/BlockChain/Form1.cs
+++ BlockChain/BlockChain/Form1.cs
@@ -21,6 +21,8 @@ namespace BlockChain
       InitializeComponent();
       ChainTester();
       dataGridView1.DataSource = RvlChain.Chain;
+      LblValid.Text = "De gegevens in deze BlockChain zijn valide!";
+      LblDataAltered.Text = "Klik op de knop om foutieve gegevens toe te voegen aan de BlockChain.";
     }
 
     private void button1_Click(object sender, EventArgs e)
@@ -28,22 +30,27 @@ namespace BlockChain
       RvlChain2 = RvlChain;
       ChainSpoofer();
       dataGridView2.DataSource = RvlChain2.Chain;
+      LblInvalid.Text = "De data in block 3, op index 2, is aangepast, waardoor de hashcode van dat Block tevens is aangepast!";
+      LblInvalid.Visible = true;
     }
     public void ChainTester()
     {
-      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Menno,reciever:Tester,waarde:9999}"));
-      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Tester,reciever:Menno,waarde:500}"));
-      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Menno,reciever:Tester,waarde:800000}"));
+      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Hogeschool Rotterdam,reciever:Belastingdienst,waarde:9999}"));
+      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Belastingdienst,reciever:Hogeschool Rotterdam,waarde:500}"));
+      RvlChain.AddBlock(new Block(DateTime.Now, null, "{sender:Hogeschool Rotterdam,reciever:Belastingdienst,waarde:8000}"));
       //Console.WriteLine(JsonConvert.SerializeObject(RvlChain, Formatting.Indented));
       Console.WriteLine($"Is chain valid?:{RvlChain.IsValid()}");
     }
     public void ChainSpoofer()
     {
       Console.WriteLine("Going to alter the data of the second in the chain");
-      var spoofBlock = new Block(DateTime.Now.AddDays(5), RvlChain2.Chain[1].Hash, "{sender:Geert Wilders,reciever:Bahama's,waarde:5000000000}");
-      RvlChain2.Chain[2] = spoofBlock;
+      string FakeData = "{sender:Belastingdienst,reciever:Hogeschool Rotterdam,waarde:50000}";
+      RvlChain.Chain[2].Data = FakeData;
+      RvlChain.Chain[2].Hash = RvlChain.Chain[2].CalculateHash();
+      //var spoofBlock = new Block(DateTime.Now.AddDays(5), RvlChain2.Chain[1].Hash, FakeData);
+      //RvlChain2.Chain[2] = spoofBlock;
 
-      Console.WriteLine($"Added the following data in to the chain: {spoofBlock.Data}");
+      //Console.WriteLine($"Added the following data in to the chain: {spoofBlock.Data}");
       Console.WriteLine($"Is chain vaild: {RvlChain2.IsValid()}");
 
       //Console.WriteLine(JsonConvert.SerializeObject(RvlChain2, Formatting.Indented));
