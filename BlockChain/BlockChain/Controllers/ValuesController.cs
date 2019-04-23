fe2a6f31f566c23c9503582e5ac8a30f793fe9a5
Skillerde6de <mennobordes@hetnet.nl>
2019-04-23 11:00:30 +0200

    Changed target framework to 2.1

--- BlockChain/BlockChain/Controllers/ValuesController.cs
+++ BlockChain/BlockChain/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace BlockChain.Controllers
     [HttpGet("{id}")]
     public ActionResult<string> Get(int id)
     {
-      return "value";
+      return "value" + id;
     }
 
     // POST api/values
