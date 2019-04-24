9da01f463b4fd7ab22a9122ff1b9e76b8b65280a
Mark Ernst <markiervc@gmail.com>
2019-04-24 15:33:35 +0200

    fixed blockobject names

--- Server/Classes/BlockObject.cs
+++ Server/Classes/BlockObject.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 
 namespace BlockChain.Classes {
     public class Data {
-        public string naam { get; set; }
+        public string Naam { get; set; }
         public string BSN { get; set; }
-        public string geb_datum { get; set; }
-        public string organisatie { get; set; }
-        public string groep { get; set; }
-        public int antecendenten { get; set; }
-        public int aanhoudingen { get; set; }
+        public string Geb_Datum { get; set; }
+        public string Organisatie { get; set; }
+        public string Groep { get; set; }
+        public int Antecendenten { get; set; }
+        public int Aanhoudingen { get; set; }
         public bool HeeftISDMaatregel { get; set; }
-        public int sepots { get; set; }
+        public int Sepots { get; set; }
         public bool HeeftOnderzoekRad { get; set; }
         public int LopendeDossiers { get; set; }
         public bool BezitUitkering { get; set; }
@@ -22,46 +22,46 @@ namespace BlockChain.Classes {
         public string LaatsteGesprek { get; set; }
 
         public Data (
-            string Naam, string BSN1, string Geb_Datum, string Organisatie, string Groep, int Antecedenten,
-            int Aanhoudingen, bool HeeftISDMaatregel1, int Sepots1, bool HeeftOnderzoekRad1, int LopendeDossiers1,
-            bool BezitUitkering1, int MeldingenRad1, bool ZitInGroepsAanpak1, bool HeeftIdBewijs1,
-            string LopendTraject1, string LaatsteGesprek1)
+            string naam, string bsn, string geb_datum, string organisatie, string groep, int antecedenten,
+            int aanhoudingen, bool heeftisdmaatregel, int sepots, bool heeftonderzoekrad, int lopendedossiers,
+            bool bezituitkering, int meldingenrad, bool zitingroepsaanpak, bool heeftidbewijs,
+            string lopendtraject, string laatstegesprek)
 
         {
-            naam = Naam;
-            BSN = BSN1;
-            geb_datum = Geb_Datum;
-            organisatie = Organisatie;
-            groep = Groep;
-            antecendenten = Antecedenten;
-            aanhoudingen = Aanhoudingen;
-            HeeftISDMaatregel = HeeftISDMaatregel1;
-            sepots = Sepots1;
-            HeeftOnderzoekRad = HeeftOnderzoekRad1;
-            LopendeDossiers = LopendeDossiers1;
-            BezitUitkering = BezitUitkering1;
-            MeldingenRad = MeldingenRad1;
-            ZitInGroepsAanpak = ZitInGroepsAanpak1;
-            HeeftIdBewijs = HeeftIdBewijs1;
-            LopendTraject = LopendTraject1;
-            LaatsteGesprek = LaatsteGesprek1;
+            Naam = naam;
+            BSN = bsn;
+            Geb_Datum = geb_datum;
+            Organisatie = organisatie;
+            Groep = groep;
+            Antecendenten = antecedenten;
+            Aanhoudingen = aanhoudingen;
+            HeeftISDMaatregel = heeftisdmaatregel;
+            Sepots = sepots;
+            HeeftOnderzoekRad = heeftonderzoekrad;
+            LopendeDossiers = lopendedossiers;
+            BezitUitkering = bezituitkering;
+            MeldingenRad = meldingenrad;
+            ZitInGroepsAanpak = zitingroepsaanpak;
+            HeeftIdBewijs = heeftidbewijs;
+            LopendTraject = lopendtraject;
+            LaatsteGesprek = laatstegesprek;
         }
 
     }
 
     public class Block {
-        public string hash_code { get; set; }
-        public string previous_hash { get; set; }
-        public string created_by { get; set; }
-        public string timestamp { get; set; }
-        public Data data { get; set; }
+        public string Hash_Code { get; set; }
+        public string Previous_Hash { get; set; }
+        public string Created_By { get; set; }
+        public string Timestamp { get; set; }
+        public Data Data { get; set; }
 
-        public Block (string Hash_code, string Previous_hash, string Created_by, string Timestamp, Data data1) {
-            hash_code = Hash_code;
-            previous_hash = Previous_hash;
-            created_by = Created_by;
-            timestamp = Timestamp;
-            data = data1;
+        public Block (string hash_code, string previous_hash, string created_by, string timestamp, Data data) {
+            Hash_Code = hash_code;
+            Previous_Hash = previous_hash;
+            Created_By = created_by;
+            Timestamp = timestamp;
+            Data = data;
         }
     }
 
