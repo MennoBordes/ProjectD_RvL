61a67dcae1b61a5cb04ff350df0637bf7bcb7c69
fabio <fjpdbruijn@hotmail.com>
2019-04-24 15:50:55 +0200

    encrypt class

---
+++ Server/Classes/letsEncrypt.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Server.Classes {
+    public class letsEncrypt {
+        public void Encrypt () {
+
+        }
+    }
+
+}
