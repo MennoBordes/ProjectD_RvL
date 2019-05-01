b1224b559661b23c19cdff9af609ef946fff6b4f
fabioshub <fjpdbruijn@hotmail.com>
2019-05-01 22:39:30 +0200

    fixed file saving

---
+++ Node/Controllers/NodeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Node.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class NodeController : ControllerBase
+  {
+    // GET api/node
+    [HttpGet()]
+    public string Get()
+    {
+        string text = System.IO.File.ReadAllText("C:/Users/Admin/Documents/ProjectD_RvL/Node/node.json");  
+        JObject result = JObject.Parse(text);
+        return result.ToString();
+    }
+  }
+}
--- Node/Program.cs
+++ Node/Program.cs
@@ -7,14 +7,40 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Node
 {
     public class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+
         public static void Main(string[] args)
         {
+
+            GetCurrentChain();
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+        static async void GetCurrentChain()
+        {
+            // Ophalen meest recente blockchain
+            var responseString = await client.GetStringAsync("http://localhost:8080/api/blockchain");
+            string text = File.ReadAllText("C:/Users/Admin/Documents/ProjectD_RvL/Node/node.json");  
+            JObject result = JObject.Parse(text);
+            JObject node = (JObject)result["node"];
+            JArray blockhain = JArray.Parse(responseString);
+            JArray item = (JArray)node["CHAIN_COPY"];
+            foreach (JObject items in blockhain)
+            {
+                item.Add(items);
+            }
+            File.WriteAllText("C:/Users/Admin/Documents/ProjectD_RvL/Node/node.json", result.ToString());
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
