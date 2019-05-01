80cb1f96b738d97f25347600c7b95574ccada7de
fabioshub <fjpdbruijn@hotmail.com>
2019-05-01 22:52:07 +0200

    fixed file saving

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,14 +7,29 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Server
 {
     public class Program
     {
+                private static readonly HttpClient client = new HttpClient();
+
         public static void Main(string[] args)
         {
+            GetNodeInfo("5000");
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        static async void GetNodeInfo(string port)
+        {
+            var responseString = await client.GetStringAsync("http://localhost:"+ port + "/api/node");
+            JObject nodeInfo = JObject.Parse(responseString);
+            System.Console.WriteLine(nodeInfo.ToString());
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
