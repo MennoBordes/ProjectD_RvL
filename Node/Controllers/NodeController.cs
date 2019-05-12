9fb17bc51b60bf960e4c48161a961931ea291dfa
Jessin Derk Rodenburg <0924698@hr.nl>
2019-05-12 22:31:24 +0200

    added basic stuff for the nodes, can't test because I'm on a different machine now

--- Node/Controllers/NodeController.cs
+++ Node/Controllers/NodeController.cs
@@ -14,6 +14,38 @@ namespace Node.Controllers {
   [Route ("api/[controller]")]
   [ApiController]
   public class NodeController : ControllerBase {
+    private string myAddress = "localhost:5001";
+    private string master = "localhost:5000"
+
+
+    public string[] getAllNodesFromMain(){
+      //connect to main server and get all the living nodes
+      //returns list of adjacent nodes
+
+      return ["127.0.0.1:5002", "127.0.0.1:5003" ];
+    }
+
+    public async Task<Object> SendMessage(string message, string address){
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(address);
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        HttpResponseMessage response = await client.GetAsync(path);
+
+        if (response.IsSuccessStatusCode){
+            product = await response.Content.ReadAsAsync<Product>();
+        }
+        return product;
+
+    }
+
+    public void AlertMaster(){
+        string msg = {address : myAddress, name:"jessin rodenburg", key: "ioweuHFD"}
+        SendMessage(msg, master)
+    }
+
     // GET api/node
     [HttpGet ()]
     public string Get () {
