097588623cac6eb09413c39a0f82dcc6cd61f30f
Skillerde6de <mennobordes@hetnet.nl>
2019-05-20 14:48:19 +0200

    Comments

--- Node/Controllers/DataController.cs
+++ Node/Controllers/DataController.cs
@@ -12,22 +12,25 @@ using Newtonsoft.Json.Linq;
 using Node.Classes.Decryption;
 using Server.Classes.Maybe;
 
-namespace Node.Controllers {
-    [Route ("api/[controller]")]
-    [ApiController]
-    public class DataController : ControllerBase {
-        // GET api/data
-        [HttpGet ()]
-        public JObject Post ([FromBody] Tupl2 tuple2) {
-            // if (tuple2.newdata["Naam"].ToString ().Length > 100) {
-            //     LetsDecrypt LetsDecrypt = new LetsDecrypt (tuple2.newdata, tuple2.keys);
-            //     return LetsDecrypt.showDecrypted ();
-            // }
-            string parentOfStartupPath = Path.GetFullPath (Path.Combine (System.AppDomain.CurrentDomain.BaseDirectory, @"../../../"));
-            string text = System.IO.File.ReadAllText (parentOfStartupPath + "/node.json");
-            JObject result = JObject.Parse (text);
-            return result;
-        }
-
+namespace Node.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DataController : ControllerBase
+  {
+    // GET api/data
+    [HttpGet()]
+    public JObject Post([FromBody] Tupl2 tuple2)
+    {
+      // if (tuple2.newdata["Naam"].ToString ().Length > 100) {
+      //     LetsDecrypt LetsDecrypt = new LetsDecrypt (tuple2.newdata, tuple2.keys);
+      //     return LetsDecrypt.showDecrypted ();
+      // }
+      string parentOfStartupPath = Path.GetFullPath(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"../../../"));
+      string text = System.IO.File.ReadAllText(parentOfStartupPath + "/node.json");
+      JObject result = JObject.Parse(text);
+      return result;
     }
+
+  }
 }
--- Server/Classes/BlockObject.1.cs
+++ Server/Classes/BlockObject.1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Server.Classes.Maybe {
+namespace Server.Classes.Maybe
+{
 
-  public class ZSM {
+  public class ZSM
+  {
     public string Antecedenten_OGR { get; set; }
     public string Aanhoudingen_OGR { get; set; }
     public string ISDMaatregel_OR { get; set; }
@@ -16,7 +18,8 @@ namespace Server.Classes.Maybe {
     public string LaatsteGesprek_POG { get; set; }
   }
 
-  public class Politie {
+  public class Politie
+  {
     public string Antecedenten_Radicalen_OGR { get; set; }
     public string Antecedenten_LokalePGA_OGR { get; set; }
     public string Antecedenten_ZSM_OGR { get; set; }
@@ -29,7 +32,8 @@ namespace Server.Classes.Maybe {
 
   }
 
-  public class OM {
+  public class OM
+  {
     public string Antecedenten_ZSM_PGR { get; set; }
     public string Sepots_ZSM_PGR { get; set; }
     public string Antecedenten_Radicalen_PGR { get; set; }
@@ -40,7 +44,8 @@ namespace Server.Classes.Maybe {
 
   }
 
-  public class Gemeente {
+  public class Gemeente
+  {
     public string BezitUitkering_ZSM_POR { get; set; }
     public string MeldingenRad_Radicalen_PO { get; set; }
     public string BezitUitkering_LokalePGA_POR { get; set; }
@@ -50,7 +55,8 @@ namespace Server.Classes.Maybe {
 
   }
 
-  public class Reclassering {
+  public class Reclassering
+  {
     public string LopendTraject_ZSM_POG { get; set; }
     public string LaatsteGesprek_ZSM_POG { get; set; }
     public string LopendTraject_Radicalen_POG { get; set; }
@@ -60,35 +66,36 @@ namespace Server.Classes.Maybe {
 
   }
 
-  public class Radicalen {
-    public string Antecedenten_OGR { get; set; }
-    public string Aanhoudingen_OGR { get; set; }
-    public string ISDMaatregel_OR { get; set; }
-    public string Antecedenten_PGR { get; set; }
-    public string Sepots_PGR { get; set; }
-    public string OnderzoekRad_PG { get; set; }
-    public string MeldingenRad_PO { get; set; }
-    public string LopendTraject_POG { get; set; }
-    public string LaatsteGesprek_POG { get; set; }
-  }
-  public class LokalePGA {
-    public string Antecedenten_OGR { get; set; }
-    public string Antecedenten_PGR { get; set; }
-    public string BezitUitkering_POR { get; set; }
-    public string ZitInGroepsAanpak_POR { get; set; }
+  // public class Radicalen {
+  //   public string Antecedenten_OGR { get; set; }
+  //   public string Aanhoudingen_OGR { get; set; }
+  //   public string ISDMaatregel_OR { get; set; }
+  //   public string Antecedenten_PGR { get; set; }
+  //   public string Sepots_PGR { get; set; }
+  //   public string OnderzoekRad_PG { get; set; }
+  //   public string MeldingenRad_PO { get; set; }
+  //   public string LopendTraject_POG { get; set; }
+  //   public string LaatsteGesprek_POG { get; set; }
+  // }
+  // public class LokalePGA {
+  //   public string Antecedenten_OGR { get; set; }
+  //   public string Antecedenten_PGR { get; set; }
+  //   public string BezitUitkering_POR { get; set; }
+  //   public string ZitInGroepsAanpak_POR { get; set; }
 
-  }
+  // }
 
-  public class Detentie {
-    public string Antecendenten { get; set; }
-    public string Aanhoudingen { get; set; }
-    public string LopendeDossiers_PGR { get; set; }
-    public string BezitUitkering_POR { get; set; }
-    public string IdBewijs { get; set; }
-    public string LopendTraject_POG { get; set; }
-    public string LaatsteGesprek_POG { get; set; }
-  }
-  public class Data {
+  // public class Detentie {
+  //   public string Antecendenten { get; set; }
+  //   public string Aanhoudingen { get; set; }
+  //   public string LopendeDossiers_PGR { get; set; }
+  //   public string BezitUitkering_POR { get; set; }
+  //   public string IdBewijs { get; set; }
+  //   public string LopendTraject_POG { get; set; }
+  //   public string LaatsteGesprek_POG { get; set; }
+  // }
+  public class Data
+  {
     public string Naam { get; set; }
     public string BSN { get; set; }
     public string Geb_Datum { get; set; }
@@ -97,9 +104,10 @@ namespace Server.Classes.Maybe {
     public Gemeente Gemeente { get; set; }
     public Reclassering Reclassering { get; set; }
 
-    public Data (
+    public Data(
       string naam, string bsn, string geb_datum, Politie Politie,
-      OM OM, Gemeente Gemeente, Reclassering Reclassering) {
+      OM OM, Gemeente Gemeente, Reclassering Reclassering)
+    {
       this.Naam = naam;
       this.BSN = bsn;
       this.Geb_Datum = geb_datum;
@@ -110,34 +118,38 @@ namespace Server.Classes.Maybe {
     }
   }
 
-  public class NewDataObject {
-    public string Naam { get; set; }
-    public string BSN { get; set; }
-    public string Geb_Datum { get; set; }
-    public ZSM Zsm { get; set; }
-    public Radicalen Radicalen { get; set; }
-    public LokalePGA LokalePGA { get; set; }
-    public Detentie Detentie { get; set; }
-  }
+  // public class NewDataObject {
+  //   public string Naam { get; set; }
+  //   public string BSN { get; set; }
+  //   public string Geb_Datum { get; set; }
+  //   public ZSM Zsm { get; set; }
+  //   public Radicalen Radicalen { get; set; }
+  //   public LokalePGA LokalePGA { get; set; }
+  //   public Detentie Detentie { get; set; }
+  // }
 
-  public class testReturn {
+  public class testReturn
+  {
     public DataObject Encrypted { get; set; }
     public DataObject Decrypted { get; set; }
 
-    public testReturn (DataObject encrypted, DataObject decrypted) {
+    public testReturn(DataObject encrypted, DataObject decrypted)
+    {
       Encrypted = encrypted;
       Decrypted = decrypted;
     }
   }
 
-  public class Block {
+  public class Block
+  {
     public string Hash_Code { get; set; }
     public string Previous_Hash { get; set; }
     public string Created_By { get; set; }
     public string Timestamp { get; set; }
     public Data Data { get; set; }
 
-    public Block (string hash_code, string previous_hash, string created_by, string timestamp, Data data) {
+    public Block(string hash_code, string previous_hash, string created_by, string timestamp, Data data)
+    {
       Hash_Code = hash_code;
       Previous_Hash = previous_hash;
       Created_By = created_by;
@@ -146,7 +158,8 @@ namespace Server.Classes.Maybe {
     }
   }
 
-  public class Tupl2 {
+  public class Tupl2
+  {
     public string keys { get; set; }
     public JObject newdata { get; set; }
   }
