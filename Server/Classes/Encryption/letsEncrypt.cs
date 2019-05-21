16f5e0fe998321cc66d4b43e7f96c60fe1e7aff0
fabio <fjpdbruijn@hotmail.com>
2019-05-21 09:58:11 +0200

    hallo ik ben het

--- Server/Classes/Encryption/letsEncrypt.cs
+++ Server/Classes/Encryption/letsEncrypt.cs
@@ -9,128 +9,103 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Server.Classes.NewBlock;
 
-namespace Server.Classes.Encryption
-{
-  public class LetsEncrypt
-  {
-    public string _publicKey;
-    public string encryptedbytes;
+namespace Server.Classes.Encryption {
+    public class LetsEncrypt {
+        public string _publicKey;
+        public string encryptedbytes;
 
-    public UnicodeEncoding _encoder = new UnicodeEncoding();
+        public UnicodeEncoding _encoder = new UnicodeEncoding ();
 
-    public JObject Encrypteddataobject { get; set; }
+        public JObject Encrypteddataobject { get; set; }
 
-<<<<<<< HEAD
         public LetsEncrypt (JObject newdata, string keys) {
             _publicKey = keys;
-            System.Console.WriteLine (newdata);
-=======
-    public LetsEncrypt(JObject newdata, string keys)
-    {
-      _publicKey = keys;
->>>>>>> 877f747d898ac0d51af4f5bf1992e87d3be0279c
 
-      JObject testObject = new JObject();
-      foreach (var item in newdata)
-      {
-        if (item.Key == "Naam" || item.Key == "BSN" || item.Key == "Geb_datum")
-        {
-          testObject.Add(item.Key, Encrypt(item.Value.ToString(), _publicKey));
+            JObject testObject = new JObject ();
+            foreach (var item in newdata) {
+                if (item.Key == "Naam" || item.Key == "BSN" || item.Key == "Geb_datum") {
+                    testObject.Add (item.Key, Encrypt (item.Value.ToString (), _publicKey));
+                }
+                if (item.Key == "Politie" || item.Key == "OM" || item.Key == "Gemeente" || item.Key == "Reclassering") {
+                    JObject jObj = JObject.FromObject (item.Value);
+                    JObject testObjectZRLD = new JObject ();
+                    foreach (var testObjectInner in jObj) {
+                        testObjectZRLD.Add (testObjectInner.Key, Encrypt (testObjectInner.Value.ToString (), _publicKey));
+                    }
+                    testObject.Add (item.Key, testObjectZRLD);
+                }
+            }
+            Encrypteddataobject = testObject;
         }
-        if (item.Key == "Politie" || item.Key == "OM" || item.Key == "Gemeente" || item.Key == "Reclassering")
-        {
-          JObject jObj = JObject.FromObject(item.Value);
-          JObject testObjectZRLD = new JObject();
-          foreach (var testObjectInner in jObj)
-          {
-            testObjectZRLD.Add(testObjectInner.Key, Encrypt(testObjectInner.Value.ToString(), _publicKey));
-          }
-          testObject.Add(item.Key, testObjectZRLD);
+
+        public JObject showEncrypted () {
+            return Encrypteddataobject;
         }
-      }
-      Encrypteddataobject = testObject;
+        // public string GetKeyString (RSAParameters publicKey) {
+
+        //     var stringWriter = new System.IO.StringWriter ();
+        //     var xmlSerializer = new System.Xml.Serialization.XmlSerializer (typeof (RSAParameters));
+        //     xmlSerializer.Serialize (stringWriter, publicKey);
+        //     return stringWriter.ToString ();
+        // }
+
+        public string Encrypt (string textToEncrypt, string publicKeyString) {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes (textToEncrypt);
+
+            using (var rsa = new RSACryptoServiceProvider (2048)) {
+                try {
+                    FromXmlString1 (rsa, publicKeyString.ToString ());
+                    var encryptedData = rsa.Encrypt (bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String (encryptedData);
+                    return base64Encrypted;
+                } finally {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public void FromXmlString1 (RSACryptoServiceProvider rsa, string xmlString) {
+            RSAParameters parameters = new RSAParameters ();
+
+            XmlDocument xmlDoc = new XmlDocument ();
+            xmlDoc.LoadXml (xmlString);
+
+            if (xmlDoc.DocumentElement.Name.Equals ("RSAKeyValue")) {
+                foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes) {
+                    switch (node.Name) {
+                        case "Modulus":
+                            parameters.Modulus = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "Exponent":
+                            parameters.Exponent = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "P":
+                            parameters.P = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "Q":
+                            parameters.Q = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "DP":
+                            parameters.DP = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "DQ":
+                            parameters.DQ = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "InverseQ":
+                            parameters.InverseQ = Convert.FromBase64String (node.InnerText);
+                            break;
+                        case "D":
+                            parameters.D = Convert.FromBase64String (node.InnerText);
+                            break;
+                    }
+                }
+            } else {
+                throw new Exception ("Invalid XML RSA key.");
+            }
+
+            rsa.ImportParameters (parameters);
+        }
+
     }
-
-    public JObject showEncrypted()
-    {
-      return Encrypteddataobject;
-    }
-    // public string GetKeyString (RSAParameters publicKey) {
-
-    //     var stringWriter = new System.IO.StringWriter ();
-    //     var xmlSerializer = new System.Xml.Serialization.XmlSerializer (typeof (RSAParameters));
-    //     xmlSerializer.Serialize (stringWriter, publicKey);
-    //     return stringWriter.ToString ();
-    // }
-
-    public string Encrypt(string textToEncrypt, string publicKeyString)
-    {
-      var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
-
-      using (var rsa = new RSACryptoServiceProvider(2048))
-      {
-        try
-        {
-          FromXmlString1(rsa, publicKeyString.ToString());
-          var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
-          var base64Encrypted = Convert.ToBase64String(encryptedData);
-          return base64Encrypted;
-        }
-        finally
-        {
-          rsa.PersistKeyInCsp = false;
-        }
-      }
-    }
-
-    public void FromXmlString1(RSACryptoServiceProvider rsa, string xmlString)
-    {
-      RSAParameters parameters = new RSAParameters();
-
-      XmlDocument xmlDoc = new XmlDocument();
-      xmlDoc.LoadXml(xmlString);
-
-      if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue"))
-      {
-        foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
-        {
-          switch (node.Name)
-          {
-            case "Modulus":
-              parameters.Modulus = Convert.FromBase64String(node.InnerText);
-              break;
-            case "Exponent":
-              parameters.Exponent = Convert.FromBase64String(node.InnerText);
-              break;
-            case "P":
-              parameters.P = Convert.FromBase64String(node.InnerText);
-              break;
-            case "Q":
-              parameters.Q = Convert.FromBase64String(node.InnerText);
-              break;
-            case "DP":
-              parameters.DP = Convert.FromBase64String(node.InnerText);
-              break;
-            case "DQ":
-              parameters.DQ = Convert.FromBase64String(node.InnerText);
-              break;
-            case "InverseQ":
-              parameters.InverseQ = Convert.FromBase64String(node.InnerText);
-              break;
-            case "D":
-              parameters.D = Convert.FromBase64String(node.InnerText);
-              break;
-          }
-        }
-      }
-      else
-      {
-        throw new Exception("Invalid XML RSA key.");
-      }
-
-      rsa.ImportParameters(parameters);
-    }
-
-  }
 
 }
