7473e7d70b230c3f8060f0d5a627405a213eb3c1
Skillerde6de <mennobordes@hetnet.nl>
2019-05-27 12:13:07 +0200

    Altered to JObject return type

--- Server/Controllers/DataController.cs
+++ Server/Controllers/DataController.cs
@@ -14,102 +14,89 @@ using Newtonsoft.Json.Linq;
 using Server.Classes.Encryption;
 using Server.Classes.NewBlock;
 
-namespace Server.Controllers {
+namespace Server.Controllers
+{
 
-  [Route ("api/[controller]")]
+  [Route("api/[controller]")]
   [ApiController]
-  public class DataController : ControllerBase {
-    HttpClient httpClient = new HttpClient ();
+  public class DataController : ControllerBase
+  {
+    HttpClient httpClient = new HttpClient();
 
     // GET: api/data
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get () {
+    public ActionResult<IEnumerable<string>> Get()
+    {
       return new string[] { "Zeehondjes" };
     }
 
     // GET: api/data/getkeys
-    [HttpGet ("getkeys/{choice}")]
-    public string Get (int choice) {
-      if (choice == 0) {
-        GenerateKeyPair keys = new GenerateKeyPair ();
-        return keys.showBoth ();
+    [HttpGet("getkeys/{choice}")]
+    public string Get(int choice)
+    {
+      if (choice == 0)
+      {
+        GenerateKeyPair keys = new GenerateKeyPair();
+        return keys.showBoth();
       }
       return "error";
     }
 
     //api/data/getcurrentchain
-    [HttpGet ("getcurrentchain")]
-    public JObject getcurrentchain () {
-      string parentOfStartupPath = Path.GetFullPath (Path.Combine (System.AppDomain.CurrentDomain.BaseDirectory, @"../../../"));
-      string current_identity = System.IO.File.ReadAllText (parentOfStartupPath + "/chainExample.json");
-      JObject current_identity_parsed = JObject.Parse (current_identity);
+    [HttpGet("getcurrentchain")]
+    public JObject getcurrentchain()
+    {
+      string parentOfStartupPath = Path.GetFullPath(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"../../../"));
+      string current_identity = System.IO.File.ReadAllText(parentOfStartupPath + "/chainExample.json");
+      JObject current_identity_parsed = JObject.Parse(current_identity);
       return current_identity_parsed;
     }
 
-    [HttpGet ("client")]
+
+    [HttpGet("client")]
     // public async ActionResult<string> Post()
-    public async Task<string> client () {
+    public async Task<JObject> client()
+    {
 
-      var url = "http://localhost:4000/api/values";
-
-      //   var url = "http://localhost:4000/api/values";
-      //   var url = "http://localhost:5001/api/values";
-      //   var url = "http://www.contoso.com/";
+      // var url = "http://localhost:4000/api/values";
+      // Used to get the entire chain
+      var url = "http://localhost:4000/api/data/getdecryptednode";
 
       ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
       ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
-      WebRequest req = WebRequest.Create (url);
+      WebRequest req = WebRequest.Create(url);
 
       req.Method = "GET";
 
       req.ContentType = "application/json; charset=utf-8";
 
-      WebResponse resp = req.GetResponse ();
+      WebResponse resp = req.GetResponse();
 
-      Stream stream = resp.GetResponseStream ();
+      Stream stream = resp.GetResponseStream();
 
-      StreamReader re = new StreamReader (stream);
+      StreamReader re = new StreamReader(stream);
 
-      String json = re.ReadToEnd ();
+      // String json = re.ReadToEnd();
+      JObject objec = JObject.Parse(re.ReadToEnd());
 
-      System.Console.WriteLine ("JSON IS DIT=======" + json);
-
-      // using (HttpClient client = new HttpClient ()) {
-
-      //   try {
-      //     var httpRequest = new HttpRequestMessage (HttpMethod.Get, url);
-      //     ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-      //     ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-      //     httpRequest.Headers.Add ("Accept", "application/json");
-      //     httpRequest.Headers.Add ("Content-Type", "application/json");
-      //     var response = await httpClient.GetAsync (url);
-      //     //   response.EnsureSuccessStatusCode();
-      //     //   string responseBody = await response.Content.ReadAsStringAsync();
-      //     //   Console.WriteLine("\nResponse: {0}\n", responseBody);
-
-      //     return response.ToString ();
-      //     // return await client.SendAsync(httpRequest).ToString();
-      //   } catch (HttpRequestException e) {
-      //     Console.WriteLine ("\nException Caught!");
-      //     Console.WriteLine ("Message: {0} \n", e.Message);
-      //     return e.Message;
-      //   }
-      // }
-      return null;
+      // System.Console.WriteLine("JSON IS DIT=======" + json);
+      return objec;
+      // return null;
     }
 
     // POST: api/data/crypto - takes input in Data class form,
     // encrypts or decrypts data
-    [HttpPost ("encryptdata")]
-    public JObject Post ([FromBody] JObject newdata) {
-      string parentOfStartupPathKeys = Path.GetFullPath (Path.Combine (System.AppDomain.CurrentDomain.BaseDirectory, @"../../../tempKeys"));
-      string keys_of_instanced = System.IO.File.ReadAllText (parentOfStartupPathKeys + "/keys.json");
-      JObject keys_of_instanced_parsed = JObject.Parse (keys_of_instanced);
-      System.Console.WriteLine (keys_of_instanced_parsed);
+    [HttpPost("encryptdata")]
+    public JObject Post([FromBody] JObject newdata)
+    {
+      string parentOfStartupPathKeys = Path.GetFullPath(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"../../../tempKeys"));
+      string keys_of_instanced = System.IO.File.ReadAllText(parentOfStartupPathKeys + "/keys.json");
+      JObject keys_of_instanced_parsed = JObject.Parse(keys_of_instanced);
+      System.Console.WriteLine(keys_of_instanced_parsed);
 
-      LetsEncrypt LetsEncrypt = new LetsEncrypt ((JObject) newdata["newdata"], keys_of_instanced_parsed);
-      return LetsEncrypt.showEncrypted ();
+      LetsEncrypt LetsEncrypt = new LetsEncrypt((JObject)newdata["newdata"], keys_of_instanced_parsed);
+      return LetsEncrypt.showEncrypted();
     }
 
   }
